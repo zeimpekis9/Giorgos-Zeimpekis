ba3e5fba39cca877dd2a4580bc1dbe633dabf463
Georgios Zeimpekis <55872441+zeimpekis9@users.noreply.github.com>
2024-08-09 19:18:43 +0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,8 +26,8 @@ const greeting = {
     "I am an Economist and have experience in Project Management 🚀"
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
-  displayGreeting: false // Set false to hide this section, defaults to true
+    "https://drive.google.com/file/d/1SxWb_tlxaxZi5RwO6vQwKmJXhMY2Tc_G/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
